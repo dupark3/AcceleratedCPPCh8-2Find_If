837cb811fd1155cf4bbb61fd5ecf763726413f55
Du Park <dupark3@gmail.com>
2018-03-01 08:47:57 -0500

    Added comments

--- find_if.hpp
+++ find_if.hpp
@@ -2,7 +2,7 @@
 template <class forwardIterator, class predicate>
 forwardIterator find_if(forwardIterator first, forwardIterator last, predicate pred){
     for (; ; ++first){
-        if (pred(*first)) return first;
-        if (first == last) return last;
+        if (pred(*first)) return first; // first instance of true value found, return this position
+        if (first == last) return last; // no instance of true value found, return last
     }
 }
